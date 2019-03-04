7b7c19817fc727ab9be3336012102b4949f8be0f
stn6mazia <nicolas.mazia.666@gmail.com>
2019-03-04 12:48:34 -0300

    begin

--- src/app/fraternitedelize/dashboard/components/get-token/get-token.component.ts
+++ src/app/fraternitedelize/dashboard/components/get-token/get-token.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TokenService } from 'src/app/fraternitedelize/services';
 import { Observable } from 'rxjs';
 import { Token } from 'src/app/fraternitedelize/shared';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-get-token',
@@ -11,12 +12,15 @@ import { Token } from 'src/app/fraternitedelize/shared';
 export class GetTokenComponent implements OnInit {
   token: Token;
   tokens: Observable<any>;
+  checked = true
 
   constructor(
+    private toastr: ToastrService,
     private tokenService: TokenService
   ) { }
 
   ngOnInit() {
+    this.checked = true
     this.token = new Token();
     this.getTokenReturn();
   }
@@ -24,10 +28,22 @@ export class GetTokenComponent implements OnInit {
   generateNewToken() {
     this.tokenService.generateToken(this.token);
     this.token = new Token();
+    this.toastr.warning(
+      'Seu token foi gerado com sucesso!',
+      'Token criado!'
+    )
+    this.toastr.warning(
+      'Após o uso o Token será deletado',
+      'Atenção!'
+    )
   }
 
   getTokenReturn() {
     this.tokens = this.tokenService.getToken();
+  }
+
+  hasToken() {
+    return this.checked = false
   }
 
 }
